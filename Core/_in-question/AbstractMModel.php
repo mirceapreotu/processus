b6850d26fbc8d81d0d34814b9d30fb2d6816eb60
Tino Ehrich <ehrich@iamholiday.com>
2011-10-25 16:11:52 +0200

    addings & movings

---
+++ Core/_in-question/AbstractMModel.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: francis
+ * Date: 8/15/11
+ * Time: 12:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+abstract class Core_Abstracts_AbstractMModel extends Core_Abstracts_AbstractModel
+{
+    public function saveInMem()
+    {
+
+    }
+}
