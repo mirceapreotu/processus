fed34d37c077961e1240fe9216fbd3396f25293c
FrancisVarga <github@varga-net.com>
2011-10-31 19:43:34 +0100

    stub auth module

--- application/php/Application/JsonRpc/V1/Pub/Auth.php
+++ application/php/Application/JsonRpc/V1/Pub/Auth.php
@@ -1,11 +1,18 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: francis
- * Date: 10/31/11
- * Time: 7:31 PM
- * To change this template use File | Settings | File Templates.
- */ 
-class Auth {
+    /**
+     * Created by JetBrains PhpStorm.
+     * User: francis
+     * Date: 10/31/11
+     * Time: 7:31 PM
+     * To change this template use File | Settings | File Templates.
+     */
 
-}
+    namespace Application\JsonRpc\V1\Pub
+    {
+        use Processus\Interfaces\InterfaceAuthModule;
+
+        class Auth implements InterfaceAuthModule
+        {
+
+        }
+    }
