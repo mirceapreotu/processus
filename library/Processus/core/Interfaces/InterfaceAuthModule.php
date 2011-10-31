e701974ea68849c675820de9ddd9926b57f7b5a2
FrancisVarga <github@varga-net.com>
2011-10-31 19:44:07 +0100

    adding interface stub

--- library/Processus/core/Interfaces/InterfaceAuthModule.php
+++ library/Processus/core/Interfaces/InterfaceAuthModule.php
@@ -1,11 +1,16 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: francis
- * Date: 10/31/11
- * Time: 7:37 PM
- * To change this template use File | Settings | File Templates.
- */ 
-class InterfaceAuthModule {
+    /**
+     * Created by JetBrains PhpStorm.
+     * User: francis
+     * Date: 10/31/11
+     * Time: 7:37 PM
+     * To change this template use File | Settings | File Templates.
+     */
 
-}
+    namespace Processus\Interfaces
+    {
+        interface InterfaceAuthModule
+        {
+
+        }
+    }
