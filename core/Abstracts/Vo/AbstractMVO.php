313602ec19a6d5b095b637ceef6f727e976d15ba
FrancisVarga <github@varga-net.com>
2011-11-01 19:10:49 +0100

    make it pretty and use new structure and server factory for memcached server

--- core/Abstracts/Vo/AbstractMVO.php
+++ core/Abstracts/Vo/AbstractMVO.php
@@ -2,7 +2,7 @@
 
 namespace Processus\Abstracts\Vo
 {
-
+    
     /**
      * Created by JetBrains PhpStorm.
      * User: francis
@@ -10,6 +10,8 @@ namespace Processus\Abstracts\Vo
      * Time: 12:39 PM
      * To change this template use File | Settings | File Templates.
      */
+    use Processus\Lib\Server\ServerFactory;
+
     abstract class AbstractMVO extends AbstractVO
     {
 
@@ -33,7 +35,7 @@ namespace Processus\Abstracts\Vo
         /**
          * @return string
          */
-        protected function getSaltValue ()
+        protected function getSaltValue()
         {
             return $this->_saltValue;
         }
@@ -41,7 +43,7 @@ namespace Processus\Abstracts\Vo
         /**
          * @return string
          */
-        protected function getHashAlgo ()
+        protected function getHashAlgo()
         {
             return $this->_hashAlgo;
         }
@@ -49,7 +51,7 @@ namespace Processus\Abstracts\Vo
         /**
          * @return string
          */
-        protected function getMemId ()
+        protected function getMemId()
         {
             return $this->_memId;
         }
@@ -58,7 +60,7 @@ namespace Processus\Abstracts\Vo
          * @param $mId
          * @return void
          */
-        public function setMemId ($mId)
+        public function setMemId($mId)
         {
             $this->_memId = $mId;
         }
@@ -66,19 +68,18 @@ namespace Processus\Abstracts\Vo
         /**
          * @return bool
          */
-        public function saveInMem ()
+        public function saveInMem()
         {
             if (! $this->getMemId()) {
                 return false;
             }
-            return $this->getMemcachedClient()->set($this->getMemId(), 
-            $this->getData(), 0);
+            return $this->getMemcachedClient()->set($this->getMemId(), $this->getData(), 0);
         }
 
         /**
          * @return bool
          */
-        public function deleteFromMem ()
+        public function deleteFromMem()
         {
             $this->getMemcachedClient()->delete($this->getMemId());
             return true;
@@ -87,35 +88,32 @@ namespace Processus\Abstracts\Vo
         /**
          * @return object
          */
-        public function getFromMem ()
+        public function getFromMem()
         {
-            $this->_data = $this->_memDataProvider = $this->getMemcachedClient()->get(
-            $this->getMemId());
+            $this->_data = $this->_memDataProvider = $this->getMemcachedClient()->get($this->getMemId());
             return $this->_memDataProvider;
         }
 
         /**
          * @throws Exception
-         * @return Memcached
+         * @return \Processus\Lib\Db\Memcached
          */
-        public function getMemcachedClient ()
+        public function getMemcachedClient()
         {
             if (! $this->_memcachedClient) {
                 try {
-                    $memId = 'default';
-                    $this->_memcachedClient = new Memcached($memId);
-                    $this->_memcachedClient->addServer($this->getMembaseHost(), 
-                    $this->getDataBucketPort());
-                } catch (Exception $error) {
-                    $couchDoc = new stdClass();
-                    $couchDoc->type = 'fatal';
-                    $couchDoc->created = time();
-                    $couchDoc->error = $error;
-                    $this->getCouchDBLogger()
-                        ->setLogType("[FATAL]" . __CLASS__ . __METHOD__)
-                        ->setData($couchDoc)
-                        ->writeLog();
+                    
+                    $memcachedConfig = array();
+                    $memcachedConfig['port'] = $this->getDataBucketPort();
+                    $memcachedConfig['host'] = $this->getMembaseHost();
+                    
+                    $this->_memcachedClient = ServerFactory::memcachedFactory($memcachedConfig);
+                
+                }
+                catch (\Exception $error) {
+                    
                     throw $error;
+                
                 }
             }
             return $this->_memcachedClient;
@@ -124,7 +122,7 @@ namespace Processus\Abstracts\Vo
         /**
          * @return string
          */
-        protected function getMembaseHost ()
+        protected function getMembaseHost()
         {
             return "127.0.0.1";
         }
@@ -132,7 +130,7 @@ namespace Processus\Abstracts\Vo
         /**
          * @return string
          */
-        protected function getDataBucketPort ()
+        protected function getDataBucketPort()
         {
             return "11280";
         }
