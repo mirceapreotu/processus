6c04bde82f35f3cccaeb9d5cfac89d4108c28436
FrancisVarga <github@varga-net.com>
2011-11-01 01:33:54 +0100

    adding stub Auth class for prototyping, shoud work

--- application/php/Application/JsonRpc/V1/Pub/Auth.php
+++ application/php/Application/JsonRpc/V1/Pub/Auth.php
@@ -6,21 +6,29 @@
      * Time: 7:31 PM
      * To change this template use File | Settings | File Templates.
      */
-
     namespace Application\JsonRpc\V1\Pub
     {
         use Processus\Interfaces\InterfaceAuthModule;
 
         class Auth implements InterfaceAuthModule
         {
+
+            private $_isAuthorized = FALSE;
+
+            /**
+             * @param $authData \Application\JsonRpc\V1\Pub\Request
+             */
             public function setAuthData($authData)
             {
-
+                $this->_isAuthorized = TRUE;
             }
-            
+
+            /**
+             * @return bool
+             */
             public function isAuthorized()
             {
-
+                return $this->_isAuthorized;
             }
         }
     }
