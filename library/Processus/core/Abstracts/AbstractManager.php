f6a8a5648e5ea8b4e0411afe5b862ac37163dbb8
Tino Ehrich <ehrich@iamholiday.com>
2011-11-01 11:07:32 +0100

    pretty

--- library/Processus/core/Abstracts/AbstractManager.php
+++ library/Processus/core/Abstracts/AbstractManager.php
@@ -1,66 +1,66 @@
 <?php
 
-namespace Processus\Abstracts
-{
-    use Processus\Lib\Db\MySQL;
-    use Processus\Lib\Db\CouchDb;
-    use Processus\Lib\Db\Memcached;
-
-    /**
-     *
-     */
-    abstract class AbstractManager
+    namespace Processus\Abstracts
     {
-
-        protected static $_mysqlInstance;
-
-        /**
-         * @return Core_Lib_Db_MySQL
-         */
-        public function getMysqlInstance ()
-        {
-            if (self::$_mysqlInstance instanceof MySQL !== TRUE) {
-                /** @var $_mysqlInstance Core_Lib_Db_MySQL */
-                self::$_mysqlInstance = MySQL::getInstance();
-            }
-            
-            return self::$_mysqlInstance;
-        }
-
-        // #########################################################
-        
-
-        protected static $_couchDbInstance;
+        use Processus\Lib\Db\MySQL;
+        use Processus\Lib\Db\CouchDb;
+        use Processus\Lib\Db\Memcached;
 
         /**
-         * @return
+         *
          */
-        public function getCouchDbInstance ()
+        abstract class AbstractManager
         {
-            if (self::$_couchDbInstance instanceof CouchDb !== TRUE) {
-                self::$_couchDbInstance = CouchDb::getInstance();
+
+            protected static $_mysqlInstance;
+
+            /**
+             * @return Core_Lib_Db_MySQL
+             */
+            public function getMysqlInstance()
+            {
+                if (self::$_mysqlInstance instanceof MySQL !== TRUE) {
+                    /** @var $_mysqlInstance Core_Lib_Db_MySQL */
+                    self::$_mysqlInstance = MySQL::getInstance();
+                }
+
+                return self::$_mysqlInstance;
             }
-            
-            return self::$_couchDbInstance;
-        }
 
-        // #########################################################
-        
+            // #########################################################
 
-        protected static $_memcachedInstance;
 
-        /**
-         * @return
-         */
-        public function getMemcachedInstance ()
-        {
-            if (self::$_memcachedInstance instanceof Memcached !== TRUE) {
-                self::$_memcachedInstance = Memcached::getInstance();
+            protected static $_couchDbInstance;
+
+            /**
+             * @return
+             */
+            public function getCouchDbInstance()
+            {
+                if (self::$_couchDbInstance instanceof CouchDb !== TRUE) {
+                    self::$_couchDbInstance = CouchDb::getInstance();
+                }
+
+                return self::$_couchDbInstance;
             }
-            
-            return self::$_memcachedInstance;
+
+            // #########################################################
+
+
+            protected static $_memcachedInstance;
+
+            /**
+             * @return
+             */
+            public function getMemcachedInstance()
+            {
+                if (self::$_memcachedInstance instanceof Memcached !== TRUE) {
+                    self::$_memcachedInstance = Memcached::getInstance();
+                }
+
+                return self::$_memcachedInstance;
+            }
         }
     }
-}
 
 ?>
