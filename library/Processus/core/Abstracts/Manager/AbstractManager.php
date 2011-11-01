8b84fe8d3eb0261d0dfef376408e591510d8986b
Tino Ehrich <ehrich@iamholiday.com>
2011-11-01 15:45:11 +0100

    starting to refactor

--- library/Processus/core/Abstracts/Manager/AbstractManager.php
+++ library/Processus/core/Abstracts/Manager/AbstractManager.php
@@ -1,71 +1,15 @@
 <?php
 
-    namespace Processus\Abstracts\Manager
+namespace Processus\Abstracts\Manager
+{
+    /**
+     *
+     */
+    use Processus\Interfaces\InterfaceDatabase;
+
+    abstract class AbstractManager implements InterfaceDatabase
     {
-        use Processus\Lib\ServerFactory;
-
-		use Processus\Lib\Db\MySQL;
-        use Processus\Lib\Db\CouchDb;
-        use Processus\Lib\Db\Memcached;
-
-        /**
-         *
-         */
-        abstract class AbstractManager
-        {
-
-            protected static $_mysqlInstance;
-
-            /**
-             * @return Core_Lib_Db_MySQL
-             */
-            public function getMysqlInstance()
-            {
-                if (self::$_mysqlInstance instanceof MySQL !== TRUE) {
-                    /** @var $_mysqlInstance Core_Lib_Db_MySQL */
-                    self::$_mysqlInstance = MySQL::getInstance();
-                }
-
-                return self::$_mysqlInstance;
-            }
-
-            // #########################################################
-
-
-            protected static $_couchDbInstance;
-
-            /**
-             * @return
-             */
-            public function getCouchDbInstance()
-            {
-                if (self::$_couchDbInstance instanceof CouchDb !== TRUE) {
-                    self::$_couchDbInstance = CouchDb::getInstance();
-                }
-
-                return self::$_couchDbInstance;
-            }
-
-            // #########################################################
-
-
-            protected static $_memcachedInstance;
-
-            /**
-             * @return
-             */
-            public function getMemcachedInstance()
-            {
-                ServerFactory::memcachedFactory();
-                ServerFactory::mysqlFactory();
-                ServerFactory::couchdbFactory();
-                
-                if (self::$_memcachedInstance instanceof Memcached !== TRUE) {
-                    self::$_memcachedInstance = Memcached::getInstance();
-                }
-
-                return self::$_memcachedInstance;
-            }
-        }
+  		  
     }
+}
 ?>
