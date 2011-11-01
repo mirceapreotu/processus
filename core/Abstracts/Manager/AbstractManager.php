552947728f67b7bd68d4737238b4a028087c2fc8
Tino Ehrich <ehrich@iamholiday.com>
2011-11-01 19:32:32 +0100

    rewriting for using with comConfig Class

--- core/Abstracts/Manager/AbstractManager.php
+++ core/Abstracts/Manager/AbstractManager.php
@@ -2,13 +2,128 @@
 
 namespace Processus\Abstracts\Manager
 {
-    /**
-     *
-     */
-    use Processus\Interfaces\InterfaceDatabase;
+    
+    use Processus\Interfaces\InterfaceComConfig;
 
-    abstract class AbstractManager implements InterfaceDatabase
+    abstract class AbstractManager
     {
+
+        /**
+         * @param InterfaceComConfig $com
+         */
+        public function fetch(InterfaceComConfig $com)
+        {
+            $results = NULL;
+            
+            if ($com->getFromCache() === TRUE) {
+                // $results = theCache($com);
+            }
+            
+            if (empty($results)) {
+                $results = $this->_fetchFromMysql($com);
+            }
+            
+            return $results;
+        }
+
+        // #########################################################
+        
+
+        /**
+         * @param InterfaceComConfig $com
+         */
+        public function fetchOne(InterfaceComConfig $com)
+        {
+            $results = NULL;
+            
+            if ($com->getFromCache() === TRUE) {
+                // $results = theCache($com);
+            }
+            
+            if (empty($results)) {
+                $results = $this->_fetchOneFromMysql($com);
+            }
+            
+            return $results;
+        }
+
+        // #########################################################
+        
+
+        /**
+         * @param InterfaceComConfig $com
+         */
+        public function fetchAll(InterfaceComConfig $com)
+        {
+            $results = NULL;
+            
+            if ($com->getFromCache() === TRUE) {
+                // $results = theCache($com);
+            }
+            
+            if (empty($results)) {
+                $results = $this->_fetchAllFromMysql($com);
+            }
+            
+            return $results;
+        }
+
+        // #########################################################
+        
+
+        /**
+         * @param InterfaceComConfig $com
+         */
+        public function insert(InterfaceComConfig $com)
+        {
+            $com->getConnector()->insert($com->getSqlTableName(), $com->getSqlParams());
+        }
+
+        // #########################################################
+        
+
+        /**
+         * @param InterfaceComConfig $com
+         */
+        public function update(InterfaceComConfig $com)
+        {
+            $com->getConnector()->update($com->getSqlTableName(), $com->getSqlConditions());
+        }
+
+        // #########################################################
+        
+
+        /**
+         * @param array $com
+         */
+        private function _fetchFromMysql($com)
+        {
+            return $com->getConnector()->fetch($com->getSqlStmt(), $com->getSqlParams());
+        }
+
+        // #########################################################
+        
+
+        /**
+         * @param array $com
+         */
+        private function _fetchOneFromMysql($com)
+        {
+            return $com->getConnector()->fetchOne($com->getSqlStmt(), $com->getSqlParams());
+        }
+
+        // #########################################################
+        
+
+        /**
+         * @param array $com
+         */
+        private function _fetchAllFromMysql($com)
+        {
+            return $com->getConnector()->fetchAll($com->getSqlStmt(), $com->getSqlParams());
+        }
+    
     }
 }
+
 ?>
