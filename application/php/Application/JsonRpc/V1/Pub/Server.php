d961acf9aa52a74e026c48f80ef4d11280488857
FrancisVarga <github@varga-net.com>
2011-11-01 02:51:59 +0100

    changing validateMethods to validateClasses

--- application/php/Application/JsonRpc/V1/Pub/Server.php
+++ application/php/Application/JsonRpc/V1/Pub/Server.php
@@ -1,18 +1,17 @@
 <?php
 
-namespace Application\JsonRpc\V1\Pub
-{
-    use Processus\Abstracts\JsonRpc\AbstractJsonRpcServer;
-
-    /**
-     *
-     */
-    class Server extends AbstractJsonRpcServer
+    namespace Application\JsonRpc\V1\Pub
     {
+        use Processus\Abstracts\JsonRpc\AbstractJsonRpcServer;
 
-        protected $_config = array(
-        'validMethods' => array('listing'));
+        class Server extends AbstractJsonRpcServer
+        {
+            protected $_config = array(
+                'validClasses' => array(
+                    'User',
+                )
+            );
+        }
     }
-}
 
 ?>
