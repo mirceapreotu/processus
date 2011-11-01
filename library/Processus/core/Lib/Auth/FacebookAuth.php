7675798ac8280fa1b1de236590286c925d9f3876
FrancisVarga <github@varga-net.com>
2011-11-01 02:49:26 +0100

    adding some stubs

--- library/Processus/core/Lib/Auth/FacebookAuth.php
+++ library/Processus/core/Lib/Auth/FacebookAuth.php
@@ -1,11 +1,26 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: francis
- * Date: 11/1/11
- * Time: 2:27 AM
- * To change this template use File | Settings | File Templates.
- */ 
-class FacebookAuth {
+    /**
+     * Created by JetBrains PhpStorm.
+     * User: francis
+     * Date: 11/1/11
+     * Time: 2:27 AM
+     * To change this template use File | Settings | File Templates.
+     */
+    namespace Processus\Lib\Auth
+    {
+        use Processus\Interfaces\InterfaceAuthModule;
 
-}
+        class FacebookAuth implements InterfaceAuthModule
+        {
+            public function setAuthData($authData)
+            {
+
+            }
+
+            public function isAuthorized()
+            {
+
+            }
+        }
+    }
+
