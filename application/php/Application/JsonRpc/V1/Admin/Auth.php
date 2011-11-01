f900e9348a8602c01910e0d9cab59132c250bb3d
FrancisVarga <github@varga-net.com>
2011-11-01 02:52:56 +0100

    adding Admin auth

--- application/php/Application/JsonRpc/V1/Admin/Auth.php
+++ application/php/Application/JsonRpc/V1/Admin/Auth.php
@@ -6,21 +6,65 @@
      * Time: 7:31 PM
      * To change this template use File | Settings | File Templates.
      */
-    namespace Application\JsonRpc\V1\Pub
+    namespace Application\JsonRpc\V1\Admin
     {
         use Processus\Interfaces\InterfaceAuthModule;
+        use Processus\Abstracts\JsonRpc\AbstractJsonRpcRequest;
+        use Processus\Lib\Auth\LowAuth;
 
-        class Auth implements InterfaceAuthModule
+        class Auth extends LowAuth
         {
 
             private $_isAuthorized = FALSE;
 
             /**
-             * @param $authData \Application\JsonRpc\V1\Pub\Request
+             * @var AbstractJsonRpcRequest
+             */
+            private $_authData;
+
+            /**
+             * @param \Processus\Abstracts\JsonRpc\AbstractJsonRpcRequest $authData
+             * @return mixed
              */
             public function setAuthData($authData)
             {
+                $this->_authData = $authData->getExtended();
+
+                if($this->getAuthId() === FALSE)
+                {
+                    $this->_isAuthorized = FALSE;
+                    return;
+                }
+
+                $sqlStmt = "SELECT * FROM users WHERE id=" . $this->_authData['id'];
+                /** @var $mysql \Processus\Lib\Db\MySQL */
+                $mysql = \Processus\Lib\Db\MySQL::getInstance();
+                $user = $mysql->fetchOne($sqlStmt);
+
+                if((boolean)$user->deauthorized === TRUE)
+                {
+                    $this->_isAuthorized = FALSE;
+                    return;
+                }
+
                 $this->_isAuthorized = TRUE;
+            }
+
+            /**
+             * @return bool|string
+             */
+            private function getAuthId()
+            {
+                if (!array_key_exists('id', $this->_authData))
+                {
+                    return FALSE;
+                }
+                if (!array_key_exists('fb', $this->_authData))
+                {
+                    return FALSE;
+                }
+
+                return (string)md5($this->_authData['fb'] . $this->_authData['id'] . 'CrowdPark4Ever!');
             }
 
             /**
