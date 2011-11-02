68c0bd9f4b849a826d1fcc91332a974dbdbc43fa
FrancisVarga <github@varga-net.com>
2011-11-02 17:02:31 +0100

    adding getter for processus config

--- core/Registry.php
+++ core/Registry.php
@@ -2,6 +2,8 @@
 
 namespace Processus
 {
+    use Processus\Lib\Vo\ProcessusConfig;
+    
     use Zend\Config\Config;
 
     /**
@@ -9,63 +11,79 @@ namespace Processus
      */
     class Registry
     {
+
         /**
          * @var Registry instance
          */
         protected static $_instance;
 
-    	/**
-    	 * @var Config
-    	 */
-    	private $config;
-
-
-        // #########################################################
-
+        /**
+         * @var Config
+         */
+        private $config;
 
         /**
-    	 * @static
-    	 * @return Registry
-    	 */
+         * @var ProcessusConfig
+         */
+        private $_processusConfig;
+
+        // #########################################################
+        
+
+        /**
+         * @static
+         * @return Registry
+         */
         public static function getInstance()
         {
-            if (self::$_instance instanceof self !== TRUE)
-            {
+            if (self::$_instance instanceof self !== TRUE) {
                 self::$_instance = new self();
                 self::$_instance->init();
             }
-
+            
             return self::$_instance;
         }
 
-
         // #########################################################
-
+        
 
         /**
-    	 * @return void
-    	 */
+         * @return void
+         */
         public function init()
         {
             $this->config = new Config(require PATH_APP . '/Application/Config/config.php');
         }
 
-
         // #########################################################
-
+        
 
         /**
-    	 * @param null $key
-    	 * @return
-    	 */
+         * @param null $key
+         * @return array | mixed
+         */
         public function getConfig($key = NULL)
         {
-            if( ! is_null($key) && $this->config->$key)
-            {
-    			return $this->config->$key;
+            if (! is_null($key) && $this->config->$key) {
+                return $this->config->$key;
             }
+            
+            return;
+        }
 
-    	    return;
+        // #########################################################
+        
+
+        /**
+         * @return \Processus\Lib\Vo\ProcessusConfig
+         */
+        public function getProcessusConfig()
+        {
+            if (! $this->_processusConfig) {
+                $this->_processusConfig = new ProcessusConfig();
+                $this->_processusConfig->setData($this->getConfig("processus"));
+            }
+            return $this->_processusConfig;
         }
     }
 }
