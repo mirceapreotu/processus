8ffbf5858660019a7845816c1d99a745187a4e84
FrancisVarga <github@varga-net.com>
2011-11-02 17:02:03 +0100

    adding Processus config wrapper

---
+++ core/Lib/Vo/ProcessusConfig.php
@@ -0,0 +1,20 @@
+<?php
+
+/** 
+ * @author francis
+ * 
+ * 
+ */
+namespace Processus\Lib\Vo
+{
+    
+    use Processus\Abstracts\Vo\AbstractVO;
+
+    class ProcessusConfig extends AbstractVO
+    {
+        //TODO - Insert your code here
+    
+
+    }
+}
+?>
