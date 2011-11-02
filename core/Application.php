9eb2384fcc4e2ec61a9504053eec4b5af873b9c3
FrancisVarga <github@varga-net.com>
2011-11-02 18:49:07 +0100

    adding structure of profile

--- core/Application.php
+++ core/Application.php
@@ -1,53 +1,65 @@
 <?php
 
-    namespace Processus
+namespace Processus
+{
+    use Processus\Lib\Bo\UserBo;
+
+	use Processus\Lib\Profiler\Profiler;
+
+    class Application
     {
 
         /**
-         *
+         * @var Application
          */
-        class Application
+        private static $_instance;
+        
+        /**
+         * @var Profiler
+         */
+        private $_profiler;
+        
+        /**
+         * @var UserBo
+         */
+        private $_userBo;
+
+        // #########################################################
+
+        /**
+         * @return \Processus\Application
+         */
+        public static function getInstance()
         {
-
-            /**
-             * @var Application
-             */
-            private static $_instance;
-
-            // #########################################################
-
-
-            /**
-             * @return \Processus\Application
-             */
-            public static function getInstance()
-            {
-                if (self::$_instance instanceof self !== TRUE) {
-                    self::$_instance = new Application();
-                    self::$_instance->init();
-                }
-
-                return self::$_instance;
+            if (self::$_instance instanceof self !== TRUE) {
+                self::$_instance = new Application();
+                self::$_instance->init();
             }
-
-            // #########################################################
-
-
-            /**
-             * @return void
-             */
-            public function init()
-            {
-            }
-
-            // #########################################################
             
-            public function getUserBo()
-            {
-                
-            }
+            return self::$_instance;
+        }
 
+        // #########################################################
+        
+        public function getUserBo()
+        {
+        
+        }
+
+        // #########################################################
+        
+        /**
+         * @return \Processus\Lib\Profiler\Profiler
+         */
+        public function getProfiler()
+        {
+            if (! $this->$_profiler) {
+                $this->$_profiler = new Profiler();
+            }
+            
+            return $this->$_profiler;
         }
     }
+}
 
 ?>
