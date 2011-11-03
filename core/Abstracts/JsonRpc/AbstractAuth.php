ba92f9b7d743b6da15001d0805a154a496d7a086
Tino Ehrich <ehrich@iamholiday.com>
2011-11-03 13:09:55 +0100

    abstract auth

---
+++ core/Abstracts/JsonRpc/AbstractAuth.php
@@ -0,0 +1,18 @@
+<?php
+
+/** 
+ * @author fightbulc
+ * 
+ * 
+ */
+namespace Processus\Abstracts\JsonRpc
+{
+
+    use Processus\Abstracts\AbstractClass;
+
+	abstract class AbstractAuth extends AbstractClass
+    {
+    }
+}
+
+?>
