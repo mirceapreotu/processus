bfb226c80e97dfbea4e8371c4553843bfbe50b5c
Tino Ehrich <ehrich@iamholiday.com>
2011-11-03 20:04:51 +0100

    helper functions file

---
+++ core/helpers.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @param array $array
+ * @return object
+ */
+function array_to_object($array)
+{
+    if (! is_array($array)) {
+        return $array;
+    }
+    
+    $object = new stdClass();
+    
+    if (is_array($array) && count($array) > 0) {
+        foreach ($array as $name => $value) {
+            $name = strtolower(trim($name));
+            if (! empty($name)) {
+                $object->$name = array_to_object($value);
+            }
+        }
+        
+        return $object;
+    }
+    else {
+        return FALSE;
+    }
+}
