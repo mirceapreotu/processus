ed1601edb239f091fd9d62036104e9d68ab654f2
Tino Ehrich <ehrich@efides.com>
2011-11-08 16:23:13 +0100

    cleaned

--- core/Lib/Auth/FacebookAuth.php
+++ core/Lib/Auth/FacebookAuth.php
@@ -1,37 +1,39 @@
 <?php
-    /**
-     * Created by JetBrains PhpStorm.
-     * User: francis
-     * Date: 11/1/11
-     * Time: 2:27 AM
-     * To change this template use File | Settings | File Templates.
-     */
-    namespace Processus\Lib\Auth
+/**
+ * Created by JetBrains PhpStorm.
+ * User: francis
+ * Date: 11/1/11
+ * Time: 2:27 AM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace Processus\Lib\Auth
+{
+    use Processus\Abstracts\JsonRpc\AbstractJsonRpcAuth;
+    
+    use Processus\Interfaces\InterfaceAuthModule;
+
+    class FacebookAuth extends AbstractJsonRpcAuth implements InterfaceAuthModule
     {
-        use Processus\Abstracts\JsonRpc\AbstractJsonRpcAuth;
 
-		use Processus\Interfaces\InterfaceAuthModule;
+        private $_isAuth;
 
-        class FacebookAuth extends AbstractJsonRpcAuth implements InterfaceAuthModule
+        /**
+         *@see Processus\Interfaces.InterfaceAuthModule::setAuthData()
+         */
+        public function setAuthData ($authData)
         {
-            
-            private $_isAuth;
-            
-            /**
-             *@see Processus\Interfaces.InterfaceAuthModule::setAuthData()
-             */
-            public function setAuthData($authData)
-            {
-                $this->_isAuth = $this->getApplication()->getUserBo()->isAuthorized();
-            }
+            $this->_isAuth = $this->getApplication()
+                ->getUserBo()
+                ->isAuthorized();
+        }
 
-            /**
-             * @see Processus\Interfaces.InterfaceAuthModule::isAuthorized()
-             */
-            public function isAuthorized()
-            {
-                return $this->_isAuth;
-            }
+        /**
+         * @see Processus\Interfaces.InterfaceAuthModule::isAuthorized()
+         */
+        public function isAuthorized ()
+        {
+            return $this->_isAuth;
         }
     }
+}
 
