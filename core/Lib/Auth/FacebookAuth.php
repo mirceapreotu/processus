7c6d0fd4534cdadd063ada9b917675c41e78c672
FrancisVarga <github@varga-net.com>
2011-11-08 16:12:40 +0100

    close #2 implements auth via. Facebook basicly

--- core/Lib/Auth/FacebookAuth.php
+++ core/Lib/Auth/FacebookAuth.php
@@ -1,37 +1,33 @@
 <?php
-    /**
-     * Created by JetBrains PhpStorm.
-     * User: francis
-     * Date: 11/1/11
-     * Time: 2:27 AM
-     * To change this template use File | Settings | File Templates.
-     */
-    namespace Processus\Lib\Auth
-    {
-        use Processus\Abstracts\JsonRpc\AbstractJsonRpcAuth;
-
-		use Processus\Interfaces\InterfaceAuthModule;
-
-        class FacebookAuth extends AbstractJsonRpcAuth implements InterfaceAuthModule
-        {
-            
-            private $_isAuth;
-            
-            /**
-             *@see Processus\Interfaces.InterfaceAuthModule::setAuthData()
-             */
-            public function setAuthData($authData)
-            {
-                $this->_isAuth = $this->getApplication()->getUserBo()->isAuthorized();
-            }
-
-            /**
-             * @see Processus\Interfaces.InterfaceAuthModule::isAuthorized()
-             */
-            public function isAuthorized()
-            {
-                return $this->_isAuth;
-            }
-        }
-    }
+/**
+ * Created by JetBrains PhpStorm.
+ * User: francis
+ * Date: 11/1/11
+ * Time: 2:27 AM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace Processus\Lib\Auth {
+	use Processus\Abstracts\JsonRpc\AbstractJsonRpcAuth;
+	
+	use Processus\Interfaces\InterfaceAuthModule;
+	
+	class FacebookAuth extends AbstractJsonRpcAuth implements InterfaceAuthModule {
+		
+		private $_isAuth;
+		
+		/**
+		 *@see Processus\Interfaces.InterfaceAuthModule::setAuthData()
+		 */
+		public function setAuthData($authData) {
+			$this->_isAuth = $this->getApplication ()->getUserBo ()->isAuthorized ();
+		}
+		
+		/**
+		 * @see Processus\Interfaces.InterfaceAuthModule::isAuthorized()
+		 */
+		public function isAuthorized() {
+			return $this->_isAuth;
+		}
+	}
+}
 
