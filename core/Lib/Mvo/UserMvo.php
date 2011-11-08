95c4bfab726c381147dace82be45a3e0a55f6254
FrancisVarga <github@varga-net.com>
2011-11-08 16:17:05 +0100

    close #7 - example how to use MVO (Memory ValueObject's)
    Extends from AbstractMvo and overwrite port/host if you need a different data bucket as the default!
    
    Find more information here => http://www.couchbase.com/products-and-services/couchbase-single-server

--- core/Lib/Mvo/UserMvo.php
+++ core/Lib/Mvo/UserMvo.php
@@ -6,36 +6,31 @@
  * 
  */
 
-namespace Processus\Lib\Mvo
-{
-    use Processus\Abstracts\Vo\AbstractMVO;
-
-    class UserMvo extends AbstractMVO
-    {
-
-        /**
-         * @return string
-         */
-        public function getFirstname()
-        {
-            return $this->getValueByKey("firstName");
-        }
-        
-        /**
-         * @return string
-         */
-        public function getLastname()
-        {
-            return $this->getValueByKey("lastName");
-        }
-        
-        /**
-         * @return string
-         */
-        public function getFullName()
-        {
-            return $this->getValueByKey("fullName");
-        }
-    }
+namespace Processus\Lib\Mvo {
+	use Processus\Abstracts\Vo\AbstractMVO;
+	
+	class UserMvo extends AbstractMVO {
+		
+		/**
+		 * @return string
+		 */
+		public function getFirstname() {
+			return $this->getValueByKey ( "firstName" );
+		}
+		
+		/**
+		 * @return string
+		 */
+		public function getLastname() {
+			return $this->getValueByKey ( "lastName" );
+		}
+		
+		/**
+		 * @return string
+		 */
+		public function getFullName() {
+			return $this->getValueByKey ( "fullName" );
+		}
+	}
 }
 ?>
