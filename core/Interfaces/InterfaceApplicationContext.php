8b0cc96fc32c94795246b8ff328fac424fc1e087
FrancisVarga <github@varga-net.com>
2011-11-16 11:21:10 +0100

    adding more dynamic :)
    #24
    #23

---
+++ core/Interfaces/InterfaceApplicationContext.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: francis
+ * Date: 11/16/11
+ * Time: 11:13 AM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace Processus\Interfaces
+{
+    interface InterfaceApplicationContext
+    {
+        public function getDefaultCache ();
+        public function getMasterMySql ();
+        public function getRegistry ();
+        public function getFacebookClient ();
+        public function getUserBo ();
+        public function getProfiler ();
+    }
+}
---
+++ core/Interfaces/InterfaceBootstrap.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: francis
+ * Date: 11/16/11
+ * Time: 11:11 AM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace Processus\ Interfaces
+{
+    interface InterfaceBootstrap
+    {
+        public function init();
+    }
+}
