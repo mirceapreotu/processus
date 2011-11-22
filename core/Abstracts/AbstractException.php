b9cb21bfb443194f814c618661d3544dc0861fea
FrancisVarga <github@varga-net.com>
2011-11-22 11:18:50 +0100

    advanced Exception handling #1 #17

--- core/Abstracts/AbstractException.php
+++ core/Abstracts/AbstractException.php
@@ -9,8 +9,71 @@
 
 namespace Processus\Abstracts
 {
-    class AbstractException extends \Exception
+    class AbstractException extends \ErrorException
     {
 
+        private $_errorData = array(
+            'userMessage' => 'Something went wrong! Please reload the site!'
+        );
+
+        /**
+         * @param object $extended
+         *
+         * @return AbstractException
+         */
+        public function setExtendData(\object $extended)
+        {
+            $this->_errorData['extended'] = $extended;
+            return $this;
+        }
+
+        /**
+         * @param string $method
+         *
+         * @return AbstractException
+         */
+        public function setMethod(\string $method)
+        {
+            $this->_errorData['method'] = $method;
+            return $this;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getMethod()
+        {
+            return $this->_errorData['method'];
+        }
+
+        /**
+         * @param string $class
+         *
+         * @return AbstractException
+         */
+        public function setClass(\string $class)
+        {
+            $this->_errorData['class'] = $class;
+            return $this;
+        }
+
+        /**
+         * @param string $message
+         *
+         * @return AbstractException
+         */
+        public function setUserMessage(\string $message)
+        {
+            $this->_errorData['userMessage'] = $message;
+            return $this;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getUserMessage()
+        {
+            return $this->_errorData['userMessage'];
+        }
     }
 }
