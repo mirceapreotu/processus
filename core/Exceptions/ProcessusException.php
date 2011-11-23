bc5b35cdcb03204d21d5312cd3bc4290200b9863
FrancisVarga <github@varga-net.com>
2011-11-23 12:39:35 +0100

    adding global ProcessusException class for framework

---
+++ core/Exceptions/ProcessusException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: francis
+ * Date: 11/23/11
+ * Time: 12:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace Processus\Exceptions
+{
+    class ProcessusException extends \Processus\Abstracts\AbstractException
+    {
+
+    }
+}
