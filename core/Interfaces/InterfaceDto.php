fc414c2f6ee15de9e36d6915681d394c25c81e48
Tino Ehrich <ehrich@efides.com>
2011-11-23 20:57:42 +0100

    added docs

--- core/Interfaces/InterfaceDto.php
+++ core/Interfaces/InterfaceDto.php
@@ -11,8 +11,16 @@ namespace Processus\Interfaces
     interface InterfaceDto
     {
 
+        /**
+         * @abstract
+         * @param $data
+         */
         public function setData ($data);
 
+        /**
+         * @abstract
+         * @return object
+         */
         public function export ();
     }
 }
