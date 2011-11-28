ce23d5bf127ccfd1a3dccf1d1e2eb9d7265f2343
FrancisVarga <github@varga-net.com>
2011-11-28 13:05:43 +0100

    adding array_prefixing helper for prefix a id list for matching mamcached id's

--- core/helpers.php
+++ core/helpers.php
@@ -2,26 +2,43 @@
 
 /**
  * @param array $array
+ *
  * @return object
  */
 function array_to_object($array)
 {
-    if (! is_array($array)) {
+    if (!is_array($array)) {
         return $array;
     }
-    
+
     $object = new stdClass();
-    
+
     if (is_array($array) && count($array) > 0) {
         foreach ($array as $name => $value) {
-            if (! empty($name)) {
+            if (!empty($name)) {
                 $object->$name = array_to_object($value);
             }
         }
-        
+
         return $object;
     }
     else {
         return FALSE;
     }
+}
+
+/**
+ * @param string $prefix
+ * @param array  $idList
+ *
+ * @return array
+ */
+function array_prefixing(string $prefix, array $idList)
+{
+    $prefixList = array();
+    foreach ($idList as $idItem)
+    {
+        $prefixList[] = $prefix . $idItem->id;
+    }
+    return $prefixList;
 }
