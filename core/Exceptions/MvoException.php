a6dc4e25849d23d8e9fe9743e2a00a611f8ed2fa
FrancisVarga <github@varga-net.com>
2011-11-29 19:13:15 +0100

    adding mvo exception

---
+++ core/Exceptions/MvoException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: francis
+ * Date: 11/29/11
+ * Time: 1:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace Processus\Exceptions
+{
+    class MvoException extends \Processus\Exceptions\ProcessusException
+    {
+
+    }
+}
