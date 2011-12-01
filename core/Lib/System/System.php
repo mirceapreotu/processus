23a832495d1c12bd03d74e04451467f6b9ac15fe
FrancisVarga <github@varga-net.com>
2011-12-01 17:19:52 +0100

    adding system class wrapper

---
+++ core/Lib/System/System.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: francis
+ * Date: 12/1/11
+ * Time: 4:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace ${NS}{ 
+class System
+{
+
+}
+}
