25a9c28d0e08cf522beb863395e36a8e7211992f
FrancisVarga <github@varga-net.com>
2011-12-06 10:35:51 +0100

    add abstract Validator class

---
+++ core/Abstracts/AbstractValidator.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: francis
+ * Date: 12/6/11
+ * Time: 12:49 AM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace ${NS}{ 
+class AbstractValidator
+{
+
+}
+}
