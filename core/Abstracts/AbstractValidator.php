569218896e7f96ae2d8fd89625c620be41be72ba
FrancisVarga <github@varga-net.com>
2011-12-06 11:01:28 +0100

    fixing ns error

--- core/Abstracts/AbstractValidator.php
+++ core/Abstracts/AbstractValidator.php
@@ -6,9 +6,29 @@
  * Time: 12:49 AM
  * To change this template use File | Settings | File Templates.
  */
-namespace ${NS}{ 
-class AbstractValidator
+namespace Processus\Abstracts
 {
+    abstract class AbstractValidator extends \Processus\Abstracts\Vo\AbstractVO
+    {
+        abstract public function validate();
 
-}
+        /**
+         * @return array|mixed
+         */
+        protected function getValidationData()
+        {
+            return $this->getValueByKey('validationData');
+        }
+
+        /**
+         * @param object $validationData
+         *
+         * @return AbstractValidator
+         */
+        public function setValidationData(\object $validationData)
+        {
+            $this->setValueByKey('validationData', $validationData);
+            return $this;
+        }
+    }
 }
