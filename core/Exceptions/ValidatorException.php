9b3a334d32aa653d582a742b41587e575f1139ce
FrancisVarga <github@varga-net.com>
2011-12-06 10:43:00 +0100

    adding validator exception

---
+++ core/Exceptions/ValidatorException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: francis
+ * Date: 12/6/11
+ * Time: 10:39 AM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace ${NS}{ 
+class ValidatorException
+{
+
+}
+}
