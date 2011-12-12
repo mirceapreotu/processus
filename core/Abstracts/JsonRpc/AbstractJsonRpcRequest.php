69e35339b440dc21f804e23d92ee81f33144d303
FrancisVarga <github@varga-net.com>
2011-12-12 02:23:05 +0100

    adding request validator

--- core/Abstracts/JsonRpc/AbstractJsonRpcRequest.php
+++ core/Abstracts/JsonRpc/AbstractJsonRpcRequest.php
@@ -42,111 +42,136 @@ namespace Processus\Abstracts\JsonRpc
         /**
          * @var array
          */
-        protected $_validatorList;
+        protected $_validatorList = array();
 
         // #########################################################
-        
 
-        public function __construct ()
+        public function __construct()
         {
             parent::__construct();
-            list ($this->_domain, $this->_class, $this->_method) = explode('.', 
-            parent::getMethod());
+            list ($this->_domain, $this->_class, $this->_method) = explode('.',
+                parent::getMethod());
+        }
+
+        /**
+         * @param \Processus\Interfaces\InterfaceValidator $validator
+         *
+         * @return \Processus\Abstracts\JsonRpc\AbstractJsonRpcRequest
+         */
+        public function addValidator(\Processus\Interfaces\InterfaceValidator $validator)
+        {
+            $validator->setValidationData($this->getRawJson());
+            $this->_validatorList[] = $validator;
+            return $this;
+        }
+
+        /**
+         * @return bool
+         */
+        public function isValidRequest()
+        {
+            /** @var $validator \Processus\Interfaces\InterfaceValidator */
+            foreach ($this->_validatorList as $validator)
+            {
+                return $validator->isValid() == TRUE ? TRUE : FALSE;
+            }
+
+            return TRUE;
         }
 
         // #########################################################
-        
+
         /**
          * @return string
          */
-        public function getClass ()
+        public function getClass()
         {
             return $this->_class;
         }
-        
+
         // #########################################################
 
         /**
          * @return string
          */
-        public function getMethod ()
+        public function getMethod()
         {
             return $this->_method;
         }
 
         // #########################################################
-        
+
 
         /**
          * @return array|mixed
          */
-        public function getExtended ()
+        public function getExtended()
         {
             return $this->_extended;
         }
 
         // #########################################################
-        
+
 
         /**
          * @param array $extended
          */
-        public function setExtended (array $extended)
+        public function setExtended(array $extended)
         {
             $this->_extended = $extended;
         }
 
         // #########################################################
-        
+
 
         /**
          * @return mixed
          */
-        public function getNamespace ()
+        public function getNamespace()
         {
             return $this->_namespace;
         }
 
         // #########################################################
-        
+
 
         /**
          * @return mixed
          */
-        public function getDomain ()
+        public function getDomain()
         {
             return $this->_domain;
         }
 
         // #########################################################
-        
+
 
         /**
          * @return string
          */
-        public function getFunctionName ()
+        public function getFunctionName()
         {
             return $this->_method;
         }
 
         // #########################################################
-        
+
 
         /**
          * @return string
          */
-        public function getSpecifiedServiceClassName ()
+        public function getSpecifiedServiceClassName()
         {
             return $this->getSpecifiedNamespace() . "\\Service\\" . $this->_class;
         }
 
         // #########################################################
-        
+
 
         /**
          * @param $specNs string
          */
-        public function setSpecifiedNamespace ($specNs)
+        public function setSpecifiedNamespace($specNs)
         {
             $this->_specifiedNamespace = $specNs;
         }
@@ -154,11 +179,11 @@ namespace Processus\Abstracts\JsonRpc
         /**
          * @return string
          */
-        public function getSpecifiedNamespace ()
+        public function getSpecifiedNamespace()
         {
             return $this->_specifiedNamespace;
         }
-    
+
     }
 }
 
