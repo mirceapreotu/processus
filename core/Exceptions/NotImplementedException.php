451ddefa62a78c3d311019ec384249c2df9dbc7e
FrancisVarga <github@varga-net.com>
2011-12-12 02:13:43 +0100

    adding validator

---
+++ core/Exceptions/NotImplementedException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: francis
+ * Date: 12/12/11
+ * Time: 1:57 AM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace ${NS}{ 
+class NotImplementedException
+{
+
+}
+}
---
+++ core/Interfaces/InterfaceValidator.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: francis
+ * Date: 12/12/11
+ * Time: 1:43 AM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace {
+    interface InterfaceValidator
+    {
+
+    }
+}
