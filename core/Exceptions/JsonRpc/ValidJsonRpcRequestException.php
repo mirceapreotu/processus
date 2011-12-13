4f7254d9d6c0909292bbb1693da84a2af1834a05
FrancisVarga <github@varga-net.com>
2011-12-13 12:06:32 +0100

    - add ValidJsonRpcRequestException

---
+++ core/Exceptions/JsonRpc/ValidJsonRpcRequestException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: francis
+ * Date: 12/12/11
+ * Time: 3:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace ${NS}{ 
+class ValidJsonRpcRequest
+{
+
+}
+}
