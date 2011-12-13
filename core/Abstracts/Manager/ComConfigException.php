ea4242ba03392e1913ae37f5caff54803cc17133
FrancisVarga <github@varga-net.com>
2011-12-13 14:45:22 +0100

    add ComConfig Exception

---
+++ core/Abstracts/Manager/ComConfigException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: francis
+ * Date: 12/13/11
+ * Time: 1:47 PM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace ${NS}{ 
+class ComConfigException
+{
+
+}
+}
