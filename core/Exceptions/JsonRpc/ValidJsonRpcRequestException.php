c78b384836c5dc9b2e9d40a3936fb42086d3734e
FrancisVarga <github@varga-net.com>
2011-12-13 12:07:58 +0100

    add some logic

--- core/Exceptions/JsonRpc/ValidJsonRpcRequestException.php
+++ core/Exceptions/JsonRpc/ValidJsonRpcRequestException.php
@@ -6,9 +6,22 @@
  * Time: 3:48 PM
  * To change this template use File | Settings | File Templates.
  */
-namespace ${NS}{ 
-class ValidJsonRpcRequest
+namespace Processus\Exceptions\JsonRpc
 {
 
-}
+    class ValidJsonRpcRequest extends \Processus\Exceptions\ProcessusException
+    {
+        /**
+         * @param string $message
+         * @param string $code
+         * @param string $severity
+         * @param string $filename
+         * @param int    $lineno
+         * @param        $previous
+         */
+        public function __construct($message = "", $code = "PRC-1000", $severity = "10", $filename = __FILE__, $lineno = __LINE__, $previous)
+        {
+            parent::__construct($message, $code, $severity, $filename, $lineno, $previous);
+        }
+    }
 }
