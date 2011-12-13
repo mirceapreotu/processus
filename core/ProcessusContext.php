c89a8583873934433fd73af3082e9ec12d753478
FrancisVarga <github@varga-net.com>
2011-12-13 12:10:16 +0100

    adding stream logger

--- core/ProcessusContext.php
+++ core/ProcessusContext.php
@@ -40,13 +40,27 @@ namespace Processus
         private static $_instance;
 
         /**
+         * @var \Zend\Log\Logger
+         */
+        private $_errorLogger;
+
+        /**
+         * @var \Zend\Log\Logger
+         */
+        private $_debugLogger;
+
+        /**
+         * @var \Zend\Log\Logger
+         */
+        private $_profilingLogger;
+
+        /**
          * @static
          * @return ProcessusContext
          */
         public static function getInstance()
         {
-            if(! self::$_instance)
-            {
+            if (!self::$_instance) {
                 self::$_instance = new ProcessusContext();
             }
 
@@ -54,43 +68,82 @@ namespace Processus
         }
 
         /**
+         * @return \Zend\Log\Logger
+         */
+        public function getDebugLogger()
+        {
+            if (!$this->_debugLogger) {
+                $streamWriter       = new \Zend\Log\Writer\Stream(PATH_ROOT . '/logs/application/debug/zend.debug.log');
+                $this->_debugLogger = new \Zend\Log\Logger($streamWriter);
+            }
+
+            return $this->_debugLogger;
+        }
+
+        /**
+         * @return \Zend\Log\Logger
+         */
+        public function getProfilingLogger()
+        {
+            if (!$this->_profilingLogger) {
+                $streamWriter           = new \Zend\Log\Writer\Stream(PATH_ROOT . 'logs/application/profiling/zend.profiling.log');
+                $this->_profilingLogger = new \Zend\Log\Logger($streamWriter);
+            }
+
+            return $this->_profilingLogger;
+        }
+
+        /**
+         * @return \Zend\Log\Logger
+         */
+        public function getErrorLogger()
+        {
+            if (!$this->_errorLogger) {
+                $streamWriter       = new \Zend\Log\Writer\Stream(PATH_ROOT . '/logs/application/error/zend.error.log');
+                $this->_errorLogger = new \Zend\Log\Logger($streamWriter);
+            }
+
+            return $this->_errorLogger;
+        }
+
+        /**
          * @return Lib\Db\Memcached
          */
-        public function getDefaultCache ()
+        public function getDefaultCache()
         {
-            if (! $this->_memcached) {
-                
+            if (!$this->_memcached) {
+
                 $config = $this->getRegistry()
                     ->getProcessusConfig()
                     ->getCouchbaseConfig()
                     ->getCouchbasePortByDatabucketKey("default");
-                
+
                 $this->_memcached = \Processus\Lib\Server\ServerFactory::memcachedFactory($config['host'], $config['port']);
             }
-            
+
             return $this->_memcached;
-        
+
         }
 
         /**
          * @return \Processus\Lib\Db\MySQL
          */
-        public function getMasterMySql ()
+        public function getMasterMySql()
         {
-            if (! $this->_mysql) {
+            if (!$this->_mysql) {
                 $this->_mysql = \Processus\Lib\Db\MySQL::getInstance();
             }
-            
+
             return $this->_mysql;
-        
+
         }
 
         /**
          * @return ProcessusRegistry
          */
-        public function getRegistry ()
+        public function getRegistry()
         {
-            if (! $this->_registry) {
+            if (!$this->_registry) {
                 $this->_registry = new ProcessusRegistry();
                 $this->_registry->init();
             }
@@ -100,23 +153,23 @@ namespace Processus
         /**
          * @return Lib\Facebook\FacebookClient
          */
-        public function getFacebookClient ()
+        public function getFacebookClient()
         {
-            if (! $this->_facebookClient) {
+            if (!$this->_facebookClient) {
                 $this->_facebookClient = new \Processus\Lib\Facebook\FacebookClient();
             }
             return $this->_facebookClient;
         }
 
         // #########################################################
-        
+
 
         /**
          * @return \Processus\Lib\Bo\UserBo
          */
-        public function getUserBo ()
+        public function getUserBo()
         {
-            if (! $this->_userBo) {
+            if (!$this->_userBo) {
                 $this->_userBo = new \Processus\Lib\Bo\UserBo();
             }
             return $this->_userBo;
@@ -127,12 +180,12 @@ namespace Processus
         /**
          * @return Profiler
          */
-        public function getProfiler ()
+        public function getProfiler()
         {
-            if (! $this->_profiler) {
+            if (!$this->_profiler) {
                 $this->_profiler = new \Processus\Lib\Profiler\ProcessusProfiler();
             }
-            
+
             return $this->_profiler;
         }
     }
