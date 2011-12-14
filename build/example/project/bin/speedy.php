947bf3a6dd88667e65ccbe53c2912b1547d0eaa0
FrancisVarga <github@varga-net.com>
2011-12-14 15:06:53 +0100

    speedy!

--- build/example/project/bin/speedy.php
+++ build/example/project/bin/speedy.php
@@ -6,3 +6,9 @@
  * Time: 11:53 AM
  * To change this template use File | Settings | File Templates.
  */
+
+#!/usr/bin/php -d memory_limit=536870912 -f
+require dirname(__FILE__)."/../src/Bootstrap.php";
+Bootstrap::init(Bootstrap::MODE_TASK, Bootstrap::SYSTEM_SVZ);
+
+Lib_Task_Runner::run();
