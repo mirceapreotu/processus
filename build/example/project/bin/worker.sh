63436e8a5ac6bc39fe06312fb0e4b91c8147c9db
FrancisVarga <github@varga-net.com>
2011-12-14 15:07:06 +0100

    worker bash script for async worker system

---
+++ build/example/project/bin/worker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+TIMEOUT_LOOP=1s
+TIMEOUT_RESTART=5s
+TIMEOUT_FAILURE=30s
+
+BIN_PATH=`dirname $0`
+
+echo "`date`;Starting worker shell loop wrapper"
+
+while true
+do
+    echo "`date` Starting PHP worker instance"
+    $BIN_PATH/run_task.php Queue_Worker -q default_svz -v 2>&1
+    RC="$?"
+    case "$RC" in
+        0)
+            echo "`date`;Worker loop success [code: 0], respawning"
+            sleep $TIMEOUT_LOOP
+            ;;
+        1)
+            echo "`date`;Worker stopped, RESTART requested [code: 1]"
+            sleep $TIMEOUT_RESTART
+            ;;
+        2)
+            echo "`date`;Worker stopped, QUIT requested [code: 2]"
+            exit
+            ;;
+        *)
+            echo "`date`;Worker stopped with errors [code: $RC], sleep & respawn"
+            sleep $TIMEOUT_FAILURE
+            ;;
+    esac
+
+done
