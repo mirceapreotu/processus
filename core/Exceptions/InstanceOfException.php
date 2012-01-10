abb21ab596ecba98bdc4fae971aa83a6c8b55113
FrancisVarga <github@varga-net.com>
2012-01-10 02:01:04 +0100

    add InstanceOfException

--- core/Exceptions/InstanceOfException.php
+++ core/Exceptions/InstanceOfException.php
@@ -6,7 +6,15 @@
  * Time: 5:23 PM
  * To change this template use File | Settings | File Templates.
  */
-class InstanceOfException
+namespace Processus\Exceptions
 {
 
+    class InstanceOfException extends \Processus\Exceptions\ProcessusException
+    {
+
+        public function __construct($message = "", $code = 1000, $severity = 10, $filename = __FILE__, $lineno = __LINE__, $previous = array())
+        {
+            parent::__construct("Class is not instanceOf", $code, $severity, $filename, $lineno, $previous);
+        }
+    }
 }
