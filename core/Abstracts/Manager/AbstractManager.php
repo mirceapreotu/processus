9d96746dd96bf03a093d7cc1203a315a135b3d29
FrancisVarga <github@varga-net.com>
2012-01-10 01:59:17 +0100

    adding ComConfig shortcut into manager

--- core/Abstracts/Manager/AbstractManager.php
+++ core/Abstracts/Manager/AbstractManager.php
@@ -203,6 +203,20 @@ namespace Processus\Abstracts\Manager
             $this->getMemcached()->insert($com->getMemId(), $results, $com->getExpiredTime());
         }
 
+        /**
+         * @param null|\Processus\Interfaces\InterfaceDatabase $connector
+         *
+         * @return ComConfig
+         */
+        protected function ccFactory(\Processus\Interfaces\InterfaceDatabase $connector = NULL)
+        {
+            if(!$connector)
+            {
+                $connector = \Processus\Lib\Db\MySQL::getInstance();
+            }
+            $comConfig = new ComConfig();
+            return $comConfig->setConnector($connector);
+        }
     }
 }
 
